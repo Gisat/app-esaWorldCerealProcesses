363b2e43db1f9d73989307bc28b54497dfa81a5d
PavelVlach <vlach1989@gmail.com>
2024-10-10 08:46:06 +0200

    Setup Table

--- src/app/processes/processes-list/page.tsx
+++ src/app/processes/processes-list/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Table } from '@mantine/core';
+import ProcessesTable from "@/components/ui/layout/ProcessesTable";
 
 const elements = [
 	{ position: 6, mass: 12.011, symbol: 'C', name: 'Carbon' },
@@ -12,26 +13,5 @@ const elements = [
 
 
 export default function Page() {
-	const rows = elements.map((element) => (
-		<Table.Tr key={element.name}>
-			<Table.Td>{element.position}</Table.Td>
-			<Table.Td>{element.name}</Table.Td>
-			<Table.Td>{element.symbol}</Table.Td>
-			<Table.Td>{element.mass}</Table.Td>
-		</Table.Tr>
-	));
-
-	return (
-		<Table>
-			<Table.Thead>
-				<Table.Tr>
-					<Table.Th>Element position</Table.Th>
-					<Table.Th>Element name</Table.Th>
-					<Table.Th>Symbol</Table.Th>
-					<Table.Th>Atomic mass</Table.Th>
-				</Table.Tr>
-			</Table.Thead>
-			<Table.Tbody>{rows}</Table.Tbody>
-		</Table>
-	);
+	return <ProcessesTable data={elements}/>
 }
---
+++ src/components/ui/layout/ProcessesTable/index.tsx
@@ -0,0 +1,34 @@
+import "./style.scss";
+import {Table} from "@mantine/core";
+
+const ProcessesTable = ({data}: Readonly<{
+    id: string;
+    type: string;
+    created: string;
+    status: string;
+}[]>) => {
+    const rows = data.map((item) => (
+        <Table.Tr key={item.name} className="worldCereal-ProcessesTable-row">
+            <Table.Td>{item.position}</Table.Td>
+            <Table.Td>{item.name}</Table.Td>
+            <Table.Td>{item.symbol}</Table.Td>
+            <Table.Td>{item.mass}</Table.Td>
+        </Table.Tr>
+    ));
+
+    return (
+        <Table highlightOnHover className="worldCereal-ProcessesTable">
+            <Table.Thead>
+                <Table.Tr className="worldCereal-ProcessesTable-row">
+                    <Table.Th>Element position</Table.Th>
+                    <Table.Th>Element name</Table.Th>
+                    <Table.Th>Symbol</Table.Th>
+                    <Table.Th>Atomic mass</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+    );
+};
+
+export default ProcessesTable;
