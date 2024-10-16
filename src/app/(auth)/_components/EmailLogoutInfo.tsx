35fa547f61f8f4d079ed0dfaa871722aa960269d
Vojtěch Dubrovský <v.dubr@hotmail.com>
2024-10-16 10:54:02 +0200

    Style homepage

--- src/app/(auth)/_components/EmailLogoutInfo.tsx
+++ src/app/(auth)/_components/EmailLogoutInfo.tsx
@@ -5,11 +5,12 @@ import Link from "next/link";
 export default ({ cookieValue, deleteCookieFunc }: { cookieValue: Unsure<string>, deleteCookieFunc: () => void }) => {
   return (
     !cookieValue ?
-      <>
-        <Link href="/account/login">
-          <Button variant="subtle" className="worldCereal-Button worldCereal-SecondaryButton">Log in</Button>
-        </Link>
-      </> :
+      // <>
+      //   <Link href="/account/login">
+      //     <Button variant="subtle" className="worldCereal-Button worldCereal-SecondaryButton">Log in</Button>
+      //   </Link>
+      // </> :
+      null :
       <>
         <span>{cookieValue}</span>
         <Link href="/api/auth/logout" onClick={() => deleteCookieFunc()}>
