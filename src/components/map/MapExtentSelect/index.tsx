9a55bbe1fcb14d1dd492b43dbea114e1b861a285
Vojtěch Dubrovský <v.dubr@hotmail.com>
2024-10-25 09:15:42 +0200

    Add setting for map extent

--- src/components/map/MapExtentSelect/index.tsx
+++ src/components/map/MapExtentSelect/index.tsx
@@ -1,12 +1,13 @@
 import DeckGL from '@deck.gl/react';
 import { GeoJsonLayer, BitmapLayer } from '@deck.gl/layers';
 import { TileLayer } from '@deck.gl/geo-layers';
+import { WebMercatorViewport } from '@deck.gl/core';
 import { useRef, useState } from 'react';
 import ExtentLayer from '@/components/map/layers/ExtentLayer';
 
 const ExtentLayerID = "ExtentLayer";
 const defaultMapSize: Array<number> = [500, 500] //in pixels
-const extentSizeInMeters = [500, 500]; //in meters
+const defaultExtentSizeInMeters = [500, 500]; //in meters
 
 type ExtentType = [] | number[] | undefined;
 
@@ -41,7 +42,7 @@ const getGeoJsonFromBbox = (bbox: ExtentType): any => {
 	}
 }
 
-export default function ({ onBboxChange, bbox, disabled, mapSize = defaultMapSize }: { mapSize?: Array<number>, disabled?: boolean, bbox?: ExtentType, onBboxChange?: (extent?: ExtentType) => void }) {
+export default function ({ longitude, latitude, onBboxChange, bbox, disabled, mapSize = defaultMapSize, extentSizeInMeters = defaultExtentSizeInMeters }: { extentSizeInMeters?: Array<number>, mapSize?: Array<number>, disabled?: boolean, bbox?: ExtentType, onBboxChange?: (extent?: ExtentType) => void, longitude: number, latitude: number }) {
 	const mapRef = useRef<any>(null);
 	const layer = bbox ? new GeoJsonLayer({
 		id: ExtentLayerID, data: getGeoJsonFromBbox(bbox), getLineColor: [0, 0, 0],
@@ -68,16 +69,29 @@ export default function ({ onBboxChange, bbox, disabled, mapSize = defaultMapSiz
 		pickable: true
 	});
 
-	const initView = bbox ? {
-		longitude: (bbox[0] + bbox[2]) / 2, latitude: (bbox[1] + bbox[3]) / 2, zoom: 13
-	} : {
-		longitude: 15, latitude: 50, zoom: 10
+
+	let fitView;
+	if (bbox) {
+		const bboxView = { longitude: (bbox[0] + bbox[2]) / 2, latitude: (bbox[1] + bbox[3]) / 2 }
+
+		const viewport = new WebMercatorViewport(bboxView);
+		fitView = viewport.fitBounds([[bbox[0], bbox[1]], [bbox[2], bbox[3]]], {
+			width: mapSize[0],
+			height: mapSize[1],
+			padding: 20
+		})
+	} else {
+		fitView = {
+			longitude: longitude || 15,
+			latitude: latitude || 50,
+			zoom: 10 //default zoom
+		}
 	}
 
-	const [mapView, setMapView] = useState(initView)
+	const [mapView, setMapView] = useState(fitView)
 
 	const onViewStateChange = ({ viewState }: { viewState: any }) => {
-		setMapView({ latitude: viewState.latitude, longitude: viewState.longitude, zoom: viewState.zoom })
+		setMapView({ latitude: viewState.latitude, longitude: viewState.longitude, zoom: !Number.isNaN(viewState.zoom) ? viewState.zoom : 10 })
 
 		// set extent layer
 		const deckHeight = mapRef?.current?.deck?.height
@@ -85,10 +99,7 @@ export default function ({ onBboxChange, bbox, disabled, mapSize = defaultMapSiz
 		const view = mapRef?.current?.deck.viewManager.views?.[0]
 		const viewport = view.makeViewport({ width: deckWidth, height: deckHeight, viewState });
 
-		// const layers = mapRef?.current?.deck?.layerManager.layers
-		// const topLeft = viewport.addMetersToLngLat([viewState.longitude, viewState.latitude], [-extentSizeInMeters[0] / 2, extentSizeInMeters[1] / 2])
 		const topRight = viewport.addMetersToLngLat([viewState.longitude, viewState.latitude], [extentSizeInMeters[0] / 2, extentSizeInMeters[1] / 2])
-		// const bottomRight = viewport.addMetersToLngLat([viewState.longitude, viewState.latitude], [extentSizeInMeters[0] / 2, -extentSizeInMeters[1] / 2])
 		const bottomLeft = viewport.addMetersToLngLat([viewState.longitude, viewState.latitude], [-extentSizeInMeters[0] / 2, -extentSizeInMeters[1] / 2])
 		if (onBboxChange) {
 			onBboxChange([...bottomLeft, ...topRight])
