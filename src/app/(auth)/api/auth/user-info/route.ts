2460e7a4cb0a628db3860753cebbc3889527a58e
Lukas Bares <baresdev697@gmail.com>
2024-11-15 16:23:09 +0100

    init page user info works

--- src/app/(auth)/api/auth/user-info/route.ts
+++ src/app/(auth)/api/auth/user-info/route.ts
@@ -1,5 +1,5 @@
+import { fetchWithSessions } from "@/app/(shared)/_ssr/handlers.sessionFetch";
 import { NextRequest, NextResponse } from "next/server";
-import { fetchWithSessions } from "../../../../(shared)/_ssr/handlers.sessionFetch";
 
 export const dynamic = 'force-dynamic'
 export const fetchCache = "force-no-store";
@@ -9,6 +9,7 @@ export async function GET(req: NextRequest) {
 
         // where to make logout
         const userInfoUrl = process.env.PID_USER_INFO as string
+        console.log(userInfoUrl)
 
         // build cookie domain of the backend app
         const response = await fetchWithSessions({
@@ -19,6 +20,7 @@ export async function GET(req: NextRequest) {
 
         // response user info back to FE client part
         const body = await response.json()
+
 
         return NextResponse.json(body)
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,30 +4,41 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Button, Container, Space, Alert, Center } from '@mantine/core';
 import { IconUser } from '@tabler/icons-react';
+import { useUserInfoFromIdentity } from "./(shared)/_hooks/useUserInfoFromIdentity";
+import PageLoader from "./(shared)/_components/PageLoader";
 
 
 export default function Home() {
 
+  const { isLoading, error, userInfoValue } = useUserInfoFromIdentity("api/auth/user-info")
   const router = useRouter()
 
+  if (isLoading)
+    return (
+      <PageLoader />
+    )
+
+  console.dir(userInfoValue)
 
   return (
     <div >
       <Space h="xl" />
-      <Container fluid h={50}>
-        <Center >
-
-          <Alert variant="transparent">
-            <p>
-              A login is required for this part of the application. After clicking the button you will be redirected to the login section.
-            </p>
-            <Space h="sm" />
-            {/* <Link href="/account/login" ><Button>Login</Button></Link> */}
-            <Link href="/api/auth/iam" ><Button autoContrast leftSection={<IconUser size={14} />}>Login</Button></Link>
-
-          </Alert>
-        </Center>
-      </Container>
+      {
+        (!userInfoValue || !userInfoValue.email) ?
+          <Container fluid h={50}>
+            <Center >
+              <Alert variant="transparent">
+                <p>
+                  A login is required for this part of the application. After clicking the button you will be redirected to the login section.
+                </p>
+                <Space h="sm" />
+                {/* <Link href="/account/login" ><Button>Login</Button></Link> */}
+                <Link href="/api/auth/iam" ><Button autoContrast leftSection={<IconUser size={14} />}>Login</Button></Link>
+              </Alert>
+            </Center>
+          </Container>
+          : null
+      }
     </div>
   );
 }
