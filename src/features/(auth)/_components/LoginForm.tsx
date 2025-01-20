ecce13696b226d8eca5797fc2a667ac663c04633
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-01-20 19:16:52 +0100

    added auth feature

---
+++ src/features/(auth)/_components/LoginForm.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Button, Card } from "@mantine/core";
+import styles from "../auth.module.css";
+import { useRouter } from "next/navigation";
+
+const LoginForm: React.FC = () => {
+
+    const router = useRouter();
+
+    return (
+        <aside className={`${styles.loginBoxWrapper}`}>
+            <Card shadow="sm" padding="lg" radius="md" withBorder className={`${styles.loginBox}`}>
+                <h1>Account Access</h1>
+                <Button onClick={() => router.push("/api/auth/iam")} className={`${styles.authButton}`} color="green">Login</Button>
+                <Button className={`${styles.authButton}`} color="gray">Register</Button>
+            </Card>
+        </aside>
+    )
+}
+
+export default LoginForm
