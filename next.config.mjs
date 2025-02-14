1393b997a0f8b5428eba7a4750b8bb326b7af394
Martin Babič <martin.babic@gisat.cz>
2025-02-14 11:06:25 +0100

    revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
 
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
+
+      '@features': path.resolve(process.cwd(), 'src', "features"), 
+      '@app': path.resolve(process.cwd(), 'src', "app"), 
+      '@tests': path.resolve(process.cwd(), 'src', "tests"), 
     };
     return config;
   },
