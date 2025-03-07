0f76dc97e0088091e5d1796bfde53d29c6e6f159
Andrii Khrystodulov <ander481@gmail.com>
2025-03-07 16:35:23 +0100

    enchance UI by disabling sections

--- src/app/(processes)/layout.tsx
+++ src/app/(processes)/layout.tsx
@@ -1,28 +1,43 @@
-"use client"
-import { Suspense } from 'react'
-import { usePathname, useSelectedLayoutSegment } from 'next/navigation'
+"use client";
 import { navbarItems } from "@features/(processes)/_constants/app";
-import NavbarItem from '@features/(shared)/_layout/_components/Navbar/NavbarItem';
-import Navbar from '@features/(shared)/_layout/_components/Navbar';
-import { ContentWrapper as Content } from '@features/(shared)/_layout/_components/Content/ContentWrapper';
+import { useIsLoggedIn } from "@features/(shared)/_hooks/user.isLogged";
+import { ContentWrapper as Content } from "@features/(shared)/_layout/_components/Content/ContentWrapper";
+import Navbar from "@features/(shared)/_layout/_components/Navbar";
+import NavbarItem from "@features/(shared)/_layout/_components/Navbar/NavbarItem";
+import { usePathname, useSelectedLayoutSegment } from "next/navigation";
+import { Suspense } from "react";
 
 export default function ProcessesLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   const currentPath = usePathname();
-  const segment = useSelectedLayoutSegment() || navbarItems[0].key
+  const segment = useSelectedLayoutSegment() || navbarItems[0].key;
 
-  const basePath = currentPath.split(`/${segment}`)[0]
-  
-    return (
-      <Suspense>
-        <Navbar activeValue={segment}>
-          {navbarItems.map((item) => <NavbarItem key={item.key} active={segment === item.key} value={item.key} title={item.title} icon={item.icon} href={`${basePath}/${item.key}`} />)}
-        </Navbar>
-        <Content>{children}</Content>
-      </Suspense>
-    );
+  const basePath = currentPath.split(`/${segment}`)[0];
+
+  const { isLoggedIn } = useIsLoggedIn("api/auth/user-info");
+
+  return (
+    <Suspense>
+      <Navbar activeValue={segment}>
+        {navbarItems.map((item) => {
+          const isDisabled = !isLoggedIn() && item.key !== "home"; // If user is not logged - disable all except "home". If user logged - all Navbar items are enabled (disable={false})
+          return (
+            <NavbarItem
+              key={item.key}
+              active={segment === item.key}
+              disabled={isDisabled}
+              value={item.key}
+              title={item.title}
+              icon={item.icon}
+              href={`${basePath}/${item.key}`}
+            />
+          );
+        })}
+      </Navbar>
+      <Content>{children}</Content>
+    </Suspense>
+  );
 }
--- src/features/(shared)/_layout/_components/Navbar/NavbarItem/index.tsx
+++ src/features/(shared)/_layout/_components/Navbar/NavbarItem/index.tsx
@@ -1,18 +1,38 @@
 "use client";
-import React, { createElement } from 'react'
-import { Tabs, rem } from '@mantine/core';
-import Link from 'next/link'
-import './style.css'
+import { Tabs, rem } from "@mantine/core";
 import { IconTypeface } from "@tabler/icons-react";
+import Link from "next/link";
+import { createElement } from "react";
+import "./style.css";
 
-const NavbarItem = ({ title, icon, href, value }: { title: string, icon: typeof IconTypeface, href: string, active: boolean, value: string }) => {
-	return (
-		<Link href={href} >
-			<Tabs.Tab className="worldCereal-NavbarItem" value={value} leftSection={createElement(icon, {
-				style: { width: rem(18), height: rem(18) },
-			})}>{title}</Tabs.Tab>
-		</Link>
-	)
-}
+const NavbarItem = ({
+  title,
+  icon,
+  href,
+  value,
+  disabled,
+}: {
+  title: string;
+  icon: typeof IconTypeface;
+  href: string;
+  active: boolean;
+  value: string;
+  disabled: boolean;
+}) => {
+  return (
+    <Link href={href}>
+      <Tabs.Tab
+        className="worldCereal-NavbarItem"
+        value={value}
+        disabled={disabled}
+        leftSection={createElement(icon, {
+          style: { width: rem(18), height: rem(18) },
+        })}
+      >
+        {title}
+      </Tabs.Tab>
+    </Link>
+  );
+};
 
-export default NavbarItem
+export default NavbarItem;
