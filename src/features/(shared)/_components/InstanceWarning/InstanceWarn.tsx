13d1a388efdbc8fbee4425c32792fd93a66e237e
Lukas Bares <47695733+barluq@users.noreply.github.com>
2025-03-12 11:27:23 +0100

    remove unneeded css

--- src/features/(shared)/_components/InstanceWarning/InstanceWarn.tsx
+++ src/features/(shared)/_components/InstanceWarning/InstanceWarn.tsx
@@ -43,7 +43,6 @@ export const InstanceWarn: React.FC = () => {
                 {data.text || 'DEV version'}
                 <button
                     onClick={() => setClosed(true)}
-                    className='crl-instance-warn-close'
                 >
                     I understand
                 </button>
