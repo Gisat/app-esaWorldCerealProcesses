801413eb23571dab7f7ea34e0f5135eff6088660
PavelVlach <vlach1989@gmail.com>
2025-07-11 09:20:40 +0200

    fix docs

--- src/app/(processes)/generate-custom-products/page.tsx
+++ src/app/(processes)/generate-custom-products/page.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import CreateProductsStep1 from '@app/(processes)/generate-custom-products/steps/1/page';
 
 /**
- * Page component for the "Download Official Products" section.
+ * Page component for the "Create Custom Products" section.
  *
- * This component serves as the entry point for the download official products process,
+ * This component serves as the entry point for the create custom products process,
  * rendering the first step of the process.
  *
  * @component
- * @returns {JSX.Element} The rendered page component for the "Download Official Products" section.
+ * @async
+ * @returns {JSX.Element} The rendered page component for the "Create Custom Products" section.
  */
 export default async function CreateCustomProductsPage() {
 	return <CreateProductsStep1 />;
--- src/app/(processes)/generate-custom-products/steps/1/page.tsx
+++ src/app/(processes)/generate-custom-products/steps/1/page.tsx
@@ -1,11 +1,12 @@
 import CreateProductsStep1Client from '@features/pages/processes/create-custom-products/steps/1/CreateProductsStep1Client';
 
 /**
- * Component representing the first step in the "Download Official Products" process.
+ * Component representing the first step in the "Create Custom Products" process.
  *
  * This step allows users to select a product collection and a specific product.
  *
  * @component
+ * @async
  * @returns {JSX.Element} The rendered component for step 1 of the process.
  */
 export default async function CreateProductsStep1() {
